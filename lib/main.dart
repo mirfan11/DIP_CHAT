53af76f529606b787671dc8e1c393f96c04ee666
mirfan11 <irfan68.im@gmail.com>
2021-05-15 22:31:05 +0700

    edit main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:dip_chat/constants.dart';
-import 'package:dip_chat/signin_screen.dart';
+import 'package:dip_chat/welcome.dart';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -20,83 +21,7 @@ class MyApp extends StatelessWidget {
           ),
         ),
       ),
-      home: WelcomeScreen(),
-    );
-  }
-}
-
-class WelcomeScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: <Widget>[
-          Expanded(
-            flex: 3,
-            child: Container(
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage("assets/perosn.jpeg"),
-                  fit: BoxFit.cover,
-                ),
-              ),
-            ),
-          ),
-          Expanded(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                RichText(
-                  textAlign: TextAlign.center,
-                  text: TextSpan(
-                    children: [
-                      TextSpan(
-                        text: "DIP CHAT\n",
-                        style: Theme.of(context).textTheme.display1,
-                      ),
-                    ],
-                  ),
-                ),
-                FittedBox(
-                  child: GestureDetector(
-                    onTap: () {
-                      Navigator.push(context, MaterialPageRoute(
-                        builder: (context) {
-                          return SignInScreen();
-                        },
-                      ));
-                    },
-                    child: Container(
-                      margin: EdgeInsets.only(bottom: 25),
-                      padding:
-                          EdgeInsets.symmetric(horizontal: 26, vertical: 16),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(25),
-                        color: kPrimaryColor,
-                      ),
-                      child: Row(
-                        children: <Widget>[
-                          Text(
-                            "Get Started",
-                            style: Theme.of(context).textTheme.button.copyWith(
-                                  color: Colors.black,
-                                ),
-                          ),
-                          SizedBox(width: 10),
-                          Icon(
-                            Icons.arrow_forward,
-                            color: Colors.black,
-                          )
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
+      home: Welcome(),
     );
   }
 }
