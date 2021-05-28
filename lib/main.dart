80984f37a43107d1ba4f4e8705da7074b992fa4c
arianza1210 <nagatoarianza12@gmail.com>
2021-05-28 19:29:57 +0700

    tambahan firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:dip_chat/constants.dart';
 import 'package:dip_chat/welcome.dart';
-
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
